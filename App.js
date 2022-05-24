d5b37d4d98d8d96bd6fa45a9440d5333f596ddd2
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-05-24 19:57:07 +0500

    improve code

--- App.js
+++ App.js
@@ -1,322 +1,221 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-  ScrollView,
-  FlatList,
-  Alert,
-} from 'react-native';
-import {Dimensions} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, FlatList, Alert }
+  from 'react-native';
+import { Dimensions } from 'react-native';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+
 const widthAfterPadding = width - 20;
+const primaryColor = '#00C750';
+
+const SizedBox = props => {
+  return (
+    <View style={{ width: props.width, height: props.height }}></View>
+  )
+}
+
+const DateDayButton = props => {
+  return (
+    <TouchableOpacity onPress={props.onPress} style={styles.dateDayButton}>
+      <Text style={{ fontSize: 16 }}>{props.text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const TextInput_Custom = props => {
+  return (
+    <View style={{ paddingVertical: 10, ...props.style }}>
+      <Text style={styles.labelText}>{props.label}</Text>
+      <TextInput
+        placeholder={props.placeholder}
+        style={{ borderBottomWidth: 1 }}
+        value={props.value}
+        onChangeText={props.onChangeText}></TextInput>
+    </View>
+  )
+}
+
+const AwesomeButton = props => {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={{ alignItems: 'center', marginLeft: 10 }}>
+      <Text
+        style={{
+          paddingHorizontal: 16,
+          backgroundColor: props.backgroundColor,
+          color: props.color,
+          fontWeight: "500",
+          paddingVertical: 8,
+          textAlign: 'center',
+          borderRadius: 7,
+        }}>
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 const App = () => {
   const [eventName, setEventName] = useState('');
   const [eventDateDay, setEventDateDay] = useState('');
   const [list, setList] = useState([]);
 
-  const renderItem = ({item}) => (
-    <Text
-      style={{
-        backgroundColor: 'green',
-        color: 'white',
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-      }}>
-      {item}
-    </Text>
-  );
+  const addToEvent = () => {
+    if (eventName == '' || eventDateDay == '') {
+      Alert.alert('Note!', 'Please fill both fields');
+      setEventName('');
+      setEventDateDay('');
+    } else {
+      setList([...list, { text: `${eventName}, ${eventDateDay}`, id: list.length + 1 }]);
+      setEventName('');
+      setEventDateDay('');
+    }
+  }
+
+  const removeAll = () => setList([])
+
+  const renderItem = ({ item }) => <Text style={styles.listItem}>{item.text}</Text>;
+
   return (
-    <SafeAreaView
-      style={{
-        paddingHorizontal: 10,
-      }}>
-      <Text
-        style={{
-          fontSize: 30,
-          textAlign: 'center',
-          color: 'green',
-        }}>
-        Event Planner App
-      </Text>
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            color: 'white',
-            backgroundColor: 'green',
-            marginTop: 20,
-          }}>
-          <Text style={styles.headerText}>M</Text>
-          <Text style={styles.headerText}>T</Text>
-          <Text style={styles.headerText}>W</Text>
-          <Text style={styles.headerText}>T</Text>
-          <Text style={styles.headerText}>F</Text>
-          <Text style={styles.headerText}>S</Text>
-          <Text style={styles.headerText}>S</Text>
+    <SafeAreaView style={{ paddingHorizontal: 10 }}>
+      <ScrollView style={{ height: height * 0.7 }}>
+        {/* Header */}
+        <Text style={styles.header}>Event Planner</Text>
+
+        {/* Calendar */}
+        <View>
+          <View style={styles.week}>
+            <Text style={styles.WeekDayAlphabet}>M</Text>
+            <Text style={styles.WeekDayAlphabet}>T</Text>
+            <Text style={styles.WeekDayAlphabet}>W</Text>
+            <Text style={styles.WeekDayAlphabet}>T</Text>
+            <Text style={styles.WeekDayAlphabet}>F</Text>
+            <Text style={styles.WeekDayAlphabet}>S</Text>
+            <Text style={styles.WeekDayAlphabet}>S</Text>
+          </View>
+          <View style={styles.week}>
+            <SizedBox width={widthAfterPadding / 7} />
+            <SizedBox width={widthAfterPadding / 7} />
+            <DateDayButton text="1" onPress={() => setEventDateDay('1 - Wednesday')} />
+            <DateDayButton text="2" onPress={() => setEventDateDay('2 - Thursday')} />
+            <DateDayButton text="3" onPress={() => setEventDateDay('3 - Friday')} />
+            <DateDayButton text="4" onPress={() => setEventDateDay('4 - Saturday')} />
+            <DateDayButton text="5" onPress={() => setEventDateDay('5 - Sunday')} />
+          </View>
+          <View style={styles.week}>
+            <DateDayButton text="6" onPress={() => setEventDateDay('6 - Monday')} />
+            <DateDayButton text="7" onPress={() => setEventDateDay('7 - Tuesday')} />
+            <DateDayButton text="8" onPress={() => setEventDateDay('8 - Wednesday')} />
+            <DateDayButton text="9" onPress={() => setEventDateDay('9 - Thursday')} />
+            <DateDayButton text="10" onPress={() => setEventDateDay('10 - Friday')} />
+            <DateDayButton text="11" onPress={() => setEventDateDay('11 - Saturday')} />
+            <DateDayButton text="12" onPress={() => setEventDateDay('12 - Sunday')} />
+          </View>
+          <View style={styles.week}>
+            <DateDayButton text="13" onPress={() => setEventDateDay('13 - Monday')} />
+            <DateDayButton text="14" onPress={() => setEventDateDay('14 - Tuesday')} />
+            <DateDayButton text="15" onPress={() => setEventDateDay('15 - Wednesday')} />
+            <DateDayButton text="16" onPress={() => setEventDateDay('16 - Thursday')} />
+            <DateDayButton text="17" onPress={() => setEventDateDay('17 - Friday')} />
+            <DateDayButton text="18" onPress={() => setEventDateDay('18 - Saturday')} />
+            <DateDayButton text="19" onPress={() => setEventDateDay('19 - Sunday')} />
+          </View>
+          <View style={styles.week}>
+            <DateDayButton text="20" onPress={() => setEventDateDay('20 - Monday')} />
+            <DateDayButton text="21" onPress={() => setEventDateDay('21 - Tuesday')} />
+            <DateDayButton text="22" onPress={() => setEventDateDay('22 - Wednesday')} />
+            <DateDayButton text="23" onPress={() => setEventDateDay('23 - Thursday')} />
+            <DateDayButton text="24" onPress={() => setEventDateDay('24 - Friday')} />
+            <DateDayButton text="25" onPress={() => setEventDateDay('25 - Saturday')} />
+            <DateDayButton text="26" onPress={() => setEventDateDay('26 - Sunday')} />
+          </View>
+          <View style={styles.week}>
+            <DateDayButton text="27" onPress={() => setEventDateDay('27 - Monday')} />
+            <DateDayButton text="28" onPress={() => setEventDateDay('28 - Tuesday')} />
+            <DateDayButton text="29" onPress={() => setEventDateDay('29 - Wednesday')} />
+            <DateDayButton text="30" onPress={() => setEventDateDay('30 - Thursday')} />
+            <SizedBox width={widthAfterPadding / 7} />
+            <SizedBox width={widthAfterPadding / 7} />
+            <SizedBox width={widthAfterPadding / 7} />
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <View style={{width: widthAfterPadding / 7}}></View>
-          <View style={{width: widthAfterPadding / 7}}></View>
-          <TouchableOpacity
-            onPress={() => setEventDateDay('1 - Wednesday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setEventDateDay('2 - Thursday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setEventDateDay('3 - Friday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setEventDateDay('4 - Saturday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setEventDateDay('5 - Sunday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>5</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>6</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>7</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>8</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>9</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>10</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>11</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>12</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>13</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>14</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>15</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>16</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>17</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>18</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>19</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <TouchableOpacity
-            onPress={() => setDateDay('20 - Monday')}
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>20</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>21</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>22</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>23</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>24</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>25</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>26</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>27</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>28</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>29</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{width: widthAfterPadding / 7, alignItems: 'center'}}>
-            <Text>30</Text>
-          </TouchableOpacity>
-          <View style={{width: widthAfterPadding / 7}}></View>
-          <View style={{width: widthAfterPadding / 7}}></View>
-          <View style={{width: widthAfterPadding / 7}}></View>
-        </View>
-      </View>
-      <View>
-        <Text
-          style={{
-            fontSize: 16,
-            color: 'black',
-            marginTop: 26,
-          }}>
-          Event Name:
-        </Text>
-        <TextInput
-          placeholder="Enter the name of the Event"
-          style={{
-            borderBottomWidth: 1,
-          }}
+
+        {/* Event Name Field */}
+        <TextInput_Custom label="Event Name:" placeholder="Enter the name of the Event"
           value={eventName}
-          onChangeText={val => setEventName(val)}></TextInput>
-      </View>
-      <View>
-        <Text
-          style={{
-            fontSize: 16,
-            color: 'black',
-            marginTop: 26,
-          }}>
-          Date/Day:
-        </Text>
-        <TextInput
-          placeholder="Select the Date/Day from Calendar"
-          style={{
-            borderBottomWidth: 1,
-          }}
-          value={eventDateDay}></TextInput>
-      </View>
-      <TouchableOpacity
-        onPress={() => {
-          if (eventDateDay != '' && eventDateDay != '') {
-            setList([...list, `${eventName},${eventDateDay}`]);
-            setEventName('');
-            setEventDateDay('');
-          } else {
-            Alert.alert('Please Fill Event Name and Date/Day');
-          }
-        }}
-        style={{
-          alignItems: 'center',
-          marginVertical: 20,
-        }}>
-        <Text
-          style={{
-            backgroundColor: 'green',
-            paddingHorizontal: 16,
-            color: 'white',
-            paddingVertical: 8,
-            textAlign: 'center',
-          }}>
-          Add to Event
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => setList([])}
-        style={{
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            backgroundColor: 'orange',
-            paddingHorizontal: 16,
-            color: 'white',
-            paddingVertical: 8,
-            textAlign: 'center',
-          }}>
-          Remove All
-        </Text>
-      </TouchableOpacity>
-      <ScrollView
-        style={{
-          marginTop: 20,
-        }}>
+          onChangeText={val => setEventName(val)}
+          style={{ marginTop: 10 }}
+        />
+
+        {/* Event Date/Day Field */}
+        <TextInput_Custom label="Date/Day:" placeholder="Select the Date/Day from Calendar"
+          value={eventDateDay}
+        />
+
+        {/* Buttons Container */}
+        <View style={{ flexDirection: "row-reverse", marginTop: 6 }}>
+          <AwesomeButton
+            backgroundColor={primaryColor} color="white" text="Add to Event"
+            onPress={addToEvent} />
+
+          <AwesomeButton
+            backgroundColor="orange" color="white" text="Remove All"
+            onPress={removeAll}
+          />
+        </View>
+      </ScrollView>
+
+      {/* Events Container */}
+      <View style={{ height: height * 0.3 - 20, borderWidth: 1, marginVertical: 10 }}>
         <FlatList
           data={list}
           renderItem={renderItem}
           keyExtractor={item => item.id}
         />
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  headerText: {
+  header: {
+    fontSize: 28,
+    fontWeight: '400',
+    textAlign: 'center',
+    paddingVertical: 15,
+    color: "black",
+  },
+  WeekDayAlphabet: {
     color: 'white',
-    fontWeight: '900',
+    backgroundColor: primaryColor,
+    fontWeight: '600',
     width: widthAfterPadding / 7,
     textAlign: 'center',
+    fontSize: 16,
+    paddingVertical: 5
   },
+  week: {
+    flexDirection: 'row'
+  },
+  labelText: { fontSize: 16, color: 'black' },
+  listItem: {
+    backgroundColor: primaryColor,
+    color: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginBottom: .5,
+    fontWeight: '600'
+  },
+  dateDayButton: {
+    width: widthAfterPadding / 7,
+    alignItems: 'center',
+    paddingVertical: 5,
+    backgroundColor: "#F0F0F0",
+    borderRadius: 3,
+    borderWidth: .5,
+    borderColor: "white"
+  }
 });
 
 export default App;
