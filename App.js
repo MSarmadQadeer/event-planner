d1ecf14f07083d1a658e1b24c99f415fd23304a0
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-05-24 15:09:08 +0500

    initialize project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
