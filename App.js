3764659b77a28a3428adf49cc39d8093930b7779
M Sarmad Qadeer <MSarmadQadeer@gmail.com>
2022-05-25 07:54:02 +0500

    add custom font

--- App.js
+++ App.js
@@ -185,6 +185,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingVertical: 15,
     color: "black",
+    fontFamily: 'PermanentMarker-Regular'
   },
   WeekDayAlphabet: {
     color: 'white',
